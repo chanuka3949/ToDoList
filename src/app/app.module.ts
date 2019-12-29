159e98b57a98bf8c044a4c7d962d89785392eca4
chanuka3949 <chanukasds3949@gmail.com>
2019-12-29 08:39:52 +0530

    Added Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ToDoListComponent } from './items/to-do-list/to-do-list.component';
 import { MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatExpansionModule, MatSnackBarModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoItemComponent,
-    ToDoListComponent
+    ToDoListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
